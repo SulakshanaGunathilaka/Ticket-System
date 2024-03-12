15ed161a2f979a5ed4b263c755975671cf8a35b4
Randika Lakmal <randikal@mexxar.com>
2024-03-12 11:31:45 +0530

    Option to close the ticket to employee

--- src/pages/Ticket1.js
+++ src/pages/Ticket1.js
@@ -70,6 +70,7 @@ export default function TicketPage1() {
   const [showModal3, setShowModal3] = useState(false);
   const [showModal4, setShowModal4] = useState(false);
   const [showModal5, setShowModal5] = useState(false);
+  const [showEmployeeCloseModel, setShowEmployeeCloseModel] = useState(false);
   const [userStatus, setUserStatus] = useState("");
   const [status, setStatus] = useState("");
   const [status1, setStatus1] = useState("");
@@ -326,6 +327,50 @@ export default function TicketPage1() {
     }
   };
 
+  const CloseEmployeeTicket = () => {
+    const ticketId = selectedTicket.id;
+    const status = "CLOSED";
+    try {
+      axios({
+        method: "put",
+        url: urls.TICKET_BASE_URL+ticketId+'/status?status='+status,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+          "Authorization": `Bearer ` + user1.jwt,
+        },
+        data: {
+
+          // id: id,
+          comment: comment,
+          status1: status
+          // createdDate: createdDate,
+          // userId: user1.user.userId,
+          // userName:  userName,
+
+
+        },
+        mode: "cors",
+      }).then((res) => {
+        console.log("response", res);
+        if (res.status == 200) {
+
+          CommonToasts.basicToast("Successfully Closed the Ticket");
+          AllTicketBasedOnUser();
+
+
+        }
+      }).catch((error) => {
+        CommonToasts.errorToast(error.message);
+        setLoading(false);
+      });
+    } catch (e) {
+      CommonToasts.errorToast(e.message);
+      setLoading(false);
+    }
+  };
+
 
   const AllTicketBasedOnUser = async () => {
     const userId = user1.user.userId;
@@ -461,6 +506,13 @@ export default function TicketPage1() {
     setSelectedTicket(data)
 
     setShowModal4(true)
+  };
+
+  const handleView7 = (data) => {
+    // ViewTicketDetails (data)
+    setSelectedTicket(data)
+    // setStatus1("CLOSED")
+    setShowEmployeeCloseModel(true)
   };
 
   const handleView2 = (id) => {
@@ -830,6 +882,27 @@ export default function TicketPage1() {
 
                         </Tooltip>
                       ) : null}
+                      { user1.user.roles[0].name === 'EMPLOYEE' ? (
+                          <Tooltip title="Close Comment" position="bottom" trigger="mouseenter">
+                            <button
+                                type="button"
+                                class="p-2 bg-white border  w-fit h-fit hover:bg-blue-200 rounded-lg shadow-md mx-1"
+
+                                onClick={() => handleView7(ticket)}
+
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-black">
+
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                              </svg>
+
+
+                            </button>
+
+                          </Tooltip>
+                      ) : null
+
+                      }
                     </div>
 
                   </div>
@@ -1454,6 +1527,91 @@ export default function TicketPage1() {
           {/* ))}  */}
 
         </>
+      ) : null}
+
+      {showEmployeeCloseModel ? (
+          <>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div
+                  className="fixed inset-0 w-full h-full bg-black opacity-40"
+                  onClick={() => setShowEmployeeCloseModel(false)}
+              ></div>
+              <div>
+
+                <div className="flex items-center min-h-screen px-4 py-8">
+                  <div className="relative bg-white rounded-lg max-w-lg p-4 mx-auto shadow dark:bg-gray-700 modal-container1">
+                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                      <h5 className="text-4xl font-bold text-blue-400">
+                        Ticket close
+                      </h5>
+                      <button
+                          type="button"
+                          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                          onClick={() => setShowEmployeeCloseModel(false)}
+                      >
+                        <svg
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                              fill-rule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </button>
+                    </div>
+
+                    <input type={"hidden"}/>
+                    <div className="w-full">
+                      <label htmlFor="description" className="block mb-2 w-96 text-sm mt-2 font-medium text-gray-900 dark:text-gray-300">
+                        Comment
+                      </label>
+                      <textarea
+                          id="description"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          onChange={(e) => setComment(e.target.value)
+                      }
+                          placeholder="Description"
+
+                      ></textarea>
+
+                    </div>
+
+
+
+
+
+
+                    <br />
+                    <button
+                        type="button"
+                        className="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                        // onClick={CloseComment}
+                        onClick={() => {
+                          CloseEmployeeTicket();
+                          setShowEmployeeCloseModel(false);
+                        }}
+                    >
+                      Close Ticket
+                    </button>
+
+
+                  </div>
+                </div>
+
+
+              </div>
+
+
+            </div>
+
+
+
+          </>
       ) : null}
 
 
