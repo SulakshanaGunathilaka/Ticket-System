c3005057d6191dbf43434417e83505c570157f7f
Randika Lakmal <randikal@mexxar.com>
2024-03-16 07:07:36 +0530

    added pop-up message before deleting the ticket

--- src/pages/Ticket1.js
+++ src/pages/Ticket1.js
@@ -72,6 +72,9 @@ export default function TicketPage1() {
   const [showModal5, setShowModal5] = useState(false);
   const [showTicketHistoryModel, setTicketHistoryModel] = useState(false);
   const [showEmployeeCloseModel, setShowEmployeeCloseModel] = useState(false);
+  const [showDeleteTicketModel, setShowDeleteTicketModel] = useState(false);
+  const [deleteStatus, set] = useState(false);
+
   const [userStatus, setUserStatus] = useState("");
   const [status, setStatus] = useState("");
   const [status1, setStatus1] = useState("");
@@ -545,6 +548,12 @@ export default function TicketPage1() {
 
 
 
+  const handleDeleteTicket = (ticketId) => {
+    setShowDeleteTicketModel(false);
+    TicketDelete(ticketId);
+
+
+  };
 
 
   const TicketDelete = (ticketId) => {
@@ -874,7 +883,7 @@ export default function TicketPage1() {
                                 type="button"
                                 className="p-2 bg-white border  w-fit h-fit hover:bg-red-200 rounded-lg shadow-md mx-1"
 
-                                onClick={() => TicketDelete(ticket.id)}
+                                onClick={() => setShowDeleteTicketModel(true)}
                             >
                               <svg
                                   xmlns="http://www.w3.org/2000/svg"
@@ -953,6 +962,79 @@ export default function TicketPage1() {
                       ) : null
 
                       }
+
+                      {showDeleteTicketModel ? (
+                          <>
+                            <div className="fixed inset-0 z-10 overflow-y-auto">
+                              <div
+                                  className="fixed inset-0 w-full h-full bg-black opacity-40"
+                                  onClick={() => setShowDeleteTicketModel(false)}
+                              ></div>
+                              <div className="flex items-center min-h-screen px-4 py-8">
+                                <div className="relative bg-white rounded-lg max-w-lg p-4 mx-auto shadow dark:bg-gray-700">
+                                  <div className="mt-3 sm:flex">
+                                    <button
+                                        type="button"
+                                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                                        onClick={() => setShowDeleteTicketModel(false)}
+                                    >
+                                      <svg
+                                          aria-hidden="true"
+                                          class="w-5 h-5"
+                                          fill="currentColor"
+                                          viewBox="0 0 20 20"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clip-rule="evenodd"
+                                        ></path>
+                                      </svg>
+                                      <span className="sr-only">Close modal</span>
+                                    </button>
+
+                                    <div className="p-6 text-center">
+                                      <svg
+                                          aria-hidden="true"
+                                          class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
+                                          fill="none"
+                                          stroke="currentColor"
+                                          viewBox="0 0 24 24"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                      </svg>
+                                      <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                        Are you sure you want to delete this ticket?
+                                      </h3>
+                                      <button
+                                          type="button"
+                                          class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                                          onClick={() => handleDeleteTicket(ticket.id)}
+                                      >
+                                        Yes, I'm sure
+                                      </button>
+                                      <button
+                                          type="button"
+                                          class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                                          onClick={() => setShowDeleteTicketModel(false)}
+                                      >
+                                        No, cancel
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </>
+                      ) : null}
+
                     </div>
 
                   </div>
@@ -960,6 +1042,7 @@ export default function TicketPage1() {
 
                 </div>
               </div>
+
 
 
             ))}
@@ -1819,6 +1902,8 @@ export default function TicketPage1() {
 
           </>
       ) : null}
+
+
 
 
 
