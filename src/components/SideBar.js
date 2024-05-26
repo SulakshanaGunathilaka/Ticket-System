df3f725ed1f8fe9af7403619916955103c6c69e1
Randika Lakmal <randikal@mexxar.com>
2024-05-26 15:13:54 +0530

    Remove User registration Page

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -87,32 +87,32 @@ export default function SideBar() {
               </Link>
             </li>
           ) : null}
-         {user1.user.roles[0].name == "ADMIN" || user1.user.roles[0].name == "IT_ADMIN"? (
-            <li className={splitLocation[1] === "createuser" ? "active" : ""}>
-              <Link
-                end
-                to="/create"
-                className="flex items-center p-2 text-base font-normal text-black rounded-lg hover:bg-sky-300"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
+         {/*{user1.user.roles[0].name == "ADMIN" || user1.user.roles[0].name == "IT_ADMIN"? (*/}
+         {/*   <li className={splitLocation[1] === "createuser" ? "active" : ""}>*/}
+         {/*     <Link*/}
+         {/*       end*/}
+         {/*       to="/create"*/}
+         {/*       className="flex items-center p-2 text-base font-normal text-black rounded-lg hover:bg-sky-300"*/}
+         {/*     >*/}
+         {/*       <svg*/}
+         {/*         xmlns="http://www.w3.org/2000/svg"*/}
+         {/*         fill="none"*/}
+         {/*         viewBox="0 0 24 24"*/}
+         {/*         stroke-width="1.5"*/}
+         {/*         stroke="currentColor"*/}
+         {/*         class="w-6 h-6"*/}
+         {/*       >*/}
+         {/*         <path*/}
+         {/*           stroke-linecap="round"*/}
+         {/*           stroke-linejoin="round"*/}
+         {/*           d="M12 4.5v15m7.5-7.5h-15"*/}
+         {/*         />*/}
+         {/*       </svg>*/}
 
-                <span className="flex-1 ml-3 whitespace-nowrap">Create User</span>
-              </Link>
-            </li>
-          ) : null}
+         {/*       <span className="flex-1 ml-3 whitespace-nowrap">Create User</span>*/}
+         {/*     </Link>*/}
+         {/*   </li>*/}
+         {/* ) : null}*/}
         {/* {user1.user.roles[0].name == "ADMIN" || user1.user.roles[0].name == "IT_ADMIN"? (
             <li className={splitLocation[1] === "edituser" ? "active" : ""}>
               <Link
