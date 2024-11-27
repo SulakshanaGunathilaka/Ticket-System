a33b2a08a30de32aadcff3c58d292a1385f7816b
LahiruDahampath <lahirur@mexxar.com>
2024-11-27 21:28:42 +0530

    ts-4 excel generating issue

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,7 +1,7 @@
 import TitleText from "../components/TitleText";
-import {React, useEffect, useRef, useState} from "react";
-import {Tooltip} from "react-tippy";
-import {CloudDownload, CloudDownloadOutlined, ForumOutlined, SearchOutlined} from "@mui/icons-material";
+import { React, useEffect, useRef, useState } from "react";
+import { Tooltip } from "react-tippy";
+import { CloudDownload, CloudDownloadOutlined, ForumOutlined, SearchOutlined } from "@mui/icons-material";
 import CommonToasts from "../common/Toasts";
 import UserService from "../services/UserService";
 import axios from "axios";
@@ -9,14 +9,15 @@ import urls from "../common/Urls";
 import AuthService from "../services/AuthenticationService";
 import ReactPaginate from "react-paginate";
 import moment from "moment";
-import {downloadExcel, DownloadTableExcel} from 'react-export-table-to-excel';
+import * as XLSX from 'xlsx';
+// import {downloadExcel, DownloadTableExcel} from 'react-export-table-to-excel';
 
 
 export default function ReportPage() {
 
     const user1 = AuthService.getCurrentUser();
 
-    const [userList,setUserList] = useState([]);
+    const [userList, setUserList] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
     const [status, setStatus] = useState('')
     const [selectedUserId, setSelectedUserId] = useState('');
@@ -25,16 +26,16 @@ export default function ReportPage() {
     const [endDate, setEndDate] = useState(null); // State for end date
     const [tickets, setTickets] = useState([]);
     const ticketData = useRef(null);
-    const header = ["Ticket Number", "Title", "Description","Type", "Status","User Name","Employee ID"];
+    const header = ["Ticket Number", "Title", "Created Date", "Description", "Type", "Status", "User Name", "Employee ID", "Accepted By", "Closed Date"];
 
     // pagination
     const [pageNumber, setPageNumber] = useState(0);
-    const ticketPerPage =10;
+    const ticketPerPage = 10;
     const pageVisited = pageNumber * ticketPerPage;
     const pageCount = Math.ceil(tickets.length / ticketPerPage);
 
-    const changePage = ({selected}) => {
-        console.log("selected page",selected);
+    const changePage = ({ selected }) => {
+        console.log("selected page", selected);
         setPageNumber(selected);
     }
 
@@ -71,15 +72,15 @@ export default function ReportPage() {
         setEndDate(selectedDate);
     };
 
-    const getReportData = async () =>{
+    const getReportData = async () => {
         try {
-            console.log("logged in user",user1.jwt);
-            console.log("selected query",searchQuery);
-            console.log("selected status",status);
-            console.log("selected User",selectedUserId);
-            console.log("selected ticket type",selectedTicketType);
-            console.log("selected starting date",startDate);
-            console.log("selected end date",endDate);
+            console.log("logged in user", user1.jwt);
+            console.log("selected query", searchQuery);
+            console.log("selected status", status);
+            console.log("selected User", selectedUserId);
+            console.log("selected ticket type", selectedTicketType);
+            console.log("selected starting date", startDate);
+            console.log("selected end date", endDate);
 
             let formatStartingDate = null;
             let formatEndingDate = null;
@@ -89,23 +90,23 @@ export default function ReportPage() {
                 search: searchQuery,
                 status: status,
                 userId: selectedUserId,
-                type:selectedTicketType,
+                type: selectedTicketType,
             }
 
-            if (startDate != null){
-                formatStartingDate =moment(startDate).format("YYYY/MM/DD");
+            if (startDate != null) {
+                formatStartingDate = moment(startDate).format("YYYY/MM/DD");
                 paramList.startDate = formatStartingDate
             }
 
-            if(endDate != null){
-                formatEndingDate =moment(endDate).format("YYYY/MM/DD");
+            if (endDate != null) {
+                formatEndingDate = moment(endDate).format("YYYY/MM/DD");
                 paramList.endDate = formatEndingDate;
             }
 
-            console.log("paramList",paramList);
+            console.log("paramList", paramList);
 
-            const response = await axios.get(urls.TICKET_REPORT,{
-                headers:{
+            const response = await axios.get(urls.TICKET_REPORT, {
+                headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
                     'Authorization': `Bearer ` + user1.jwt,
@@ -113,11 +114,29 @@ export default function ReportPage() {
                 params: paramList,
             });
 
+            const updatedTickets = response.data.body.map(ticket => {
+                let acceptedBy = "N/A";
+
+                // Check if there is a history and extract the acceptedBy value
+                if (ticket.history) {
+                    const acceptedHistory = ticket.history.find(h => h.newStatus === "ACCEPTED");
+                    if (acceptedHistory) {
+                        acceptedBy = `${acceptedHistory.updatedUser.firstName} ${acceptedHistory.updatedUser.lastName}`;
+                    }
+                }
+
+                // Return the updated ticket with acceptedBy included
+                return {
+                    ...ticket,
+                    acceptedBy,
+                };
+            });
+
             console.log(response.data.body);
-            setTickets(response.data.body);
+            setTickets(updatedTickets);
 
 
-        }catch (err){
+        } catch (err) {
             console.log(err)
         }
     }
@@ -131,7 +150,7 @@ export default function ReportPage() {
             try {
                 const response = await UserService.getAllUsers(1, 5);
                 setUserList(response.data.body);
-            }catch(e){
+            } catch (e) {
                 CommonToasts.errorToast(e.message);
             }
         }
@@ -146,7 +165,7 @@ export default function ReportPage() {
 
 
     // Download data
-    const handleExport= () =>{
+    const handleExport = () => {
         // const worksheet = XLSX.utils.json_to_sheet(tickets);
         // const workbook = XLSX.utils.book_new();
         // XLSX.utils.book_append_sheet(workbook,worksheet,'Data');
@@ -156,31 +175,64 @@ export default function ReportPage() {
         // XLSX.writeFile(workbook,'export.xlsx');
     };
 
-    function handleDownloadExcel(){
-        downloadExcel({
-            fileName:"data",
-            sheet:"data-export",
-            tablePayload:{
-                header,
-                body: tickets.map((data) => {
-                    // Destructure relevant properties from ticket object
-                    const { id, title, description, type, status, user } = data;
+    function handleDownloadExcel() {
+        // Define headers for the Excel file
+        const header = ["ID", "Title", "Created Date", "Description", "Type", "Status", "Full Name", "Employee ID", "Accepted By", "Closed Date"];
 
-                    // Access nested user properties
-                    const { firstName, lastName, employeeId } = user;
+        // Map the data to match the headers
+        const data = tickets.map((data) => {
+            const { id, title, createdDate, description, type, status, user, history } = data;
 
-                    // Construct the data row with desired properties
-                    return { id, title, description, type, status, fullName: `${firstName} ${lastName}`, employeeId }
-                })
+            const { firstName, lastName, employeeId } = user;
+
+            let acceptedBy = "N/A";
+            let closedDate = "N/A";
+
+            if (history) {
+                const acceptedHistory = history.find((h) => h.newStatus === "ACCEPTED");
+                if (acceptedHistory) {
+                    acceptedBy = `${acceptedHistory.updatedUser.firstName} ${acceptedHistory.updatedUser.lastName}`;
+                }
+
+                const closedHistory = history.find((h) => h.newStatus === "CLOSED");
+                if (closedHistory) {
+                    closedDate = moment(closedHistory.date).format("YYYY-MM-DD");
+                }
             }
+
+            return {
+                ID: id,
+                Title: title,
+                "Created Date": moment(createdDate).format("YYYY-MM-DD"),
+                Description: description,
+                Type: type,
+                Status: status,
+                "Full Name": `${firstName} ${lastName}`,
+                "Employee ID": employeeId,
+                "Accepted By": acceptedBy,
+                "Closed Date": closedDate,
+            };
         });
+
+        // Create a new workbook and worksheet
+        const workbook = XLSX.utils.book_new();
+        const worksheet = XLSX.utils.json_to_sheet(data);
+
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Tickets");
+
+        const currentDate = new Date();
+        const formattedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+
+        // Write workbook to file
+        XLSX.writeFile(workbook, `ticket-report-${formattedDate}.xlsx`);
     }
 
 
 
 
 
-    return(
+    return (
         <div className=" bg-gray h-fit w-full">
             <TitleText titleText="Reports" />
             <div className=" absolute mt-4 h-[82%] w-[82%] bg-white rounded-lg border border-gray-200 shadow-md">
@@ -203,7 +255,7 @@ export default function ReportPage() {
                             <Tooltip title="Search" position="bottom" trigger="mouseenter">
                                 <button
                                     onClick={getReportData}>
-                                    <SearchOutlined/>
+                                    <SearchOutlined />
 
                                 </button>
                             </Tooltip>
@@ -241,7 +293,7 @@ export default function ReportPage() {
                                 className="bg-white border border-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-44 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 // onClick={handleSearchStatus}
                                 onChange={(e) => setSelectedUserId(e.target.value)}
-                                // value={tickets?.user?.id}
+                            // value={tickets?.user?.id}
                             >
                                 <option value="">Select User...</option>
                                 <option value="">All Users</option>
@@ -305,7 +357,14 @@ export default function ReportPage() {
                                 max={new Date().toISOString().split('T')[0]}
                             />
                         </div>
-
+                        {/* Export Button */}
+                        <div className="p-1 bg-white w-10 h-10 hover:bg-gray-200 rounded-lg shadow-md mx-1 ">
+                            <Tooltip title="Export to Excel" position="bottom" trigger="mouseenter">
+                                <button onClick={handleDownloadExcel}>
+                                    <CloudDownloadOutlined />
+                                </button>
+                            </Tooltip>
+                        </div>
                     </div>
                 </div>
 
@@ -317,66 +376,61 @@ export default function ReportPage() {
                 {/*    sheet="sheet 1"*/}
                 {/*    buttonText="Expot"*/}
                 {/*/>*/}
-                <div>
-                    <div className="flex justify-end mr-5">
-                        <Tooltip title="Export Excel" position="bottom" trigger="mouseenter">
-                            <button onClick={handleDownloadExcel}>
-                                <div className="flex  ">
-                                    Export to Excel
-                                    <CloudDownloadOutlined/>
-
-                                </div>
-
-                            </button>
-                        </Tooltip>
+                {/*body*/}
+                <div className="relative max-h-[calc(100vh-200px)] flex flex-col">
+                    {/* Tickets Table */}
+                    <div className="overflow-auto flex-1">
+                        <table className="table-auto w-full border-collapse table-layout-fixed">
+                            <thead>
+                                <tr>
+                                    <th className="p-4 text-sm text-gray-600 border-b">ID</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Title</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Created Date</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Description</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Type</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Status</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Full Name</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Employee ID</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Accepted By</th>
+                                    <th className="p-4 text-sm text-gray-600 border-b">Closed Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {tickets.slice(pageVisited, pageVisited + ticketPerPage).map((ticket) => (
+                                    <tr key={ticket.id} className="border-b">
+                                        <td className="p-4 text-sm">{ticket.id}</td>
+                                        <td className="p-4 text-sm break-words">{ticket.title}</td>
+                                        <td className="p-4 text-sm whitespace-nowrap">{moment(ticket.createdDate).format("YYYY-MM-DD")}</td>
+                                        <td className="p-4 text-sm break-words">{ticket.description}</td>
+                                        <td className="p-4 text-sm">{ticket.type}</td>
+                                        <td className="p-4 text-sm">{ticket.status}</td>
+                                        <td className="p-4 text-sm">{ticket.user.firstName} {ticket.user.lastName}</td>
+                                        <td className="p-4 text-sm">{ticket.user.employeeId}</td>
+                                        <td className="p-4 text-sm">{ticket.acceptedBy}</td>
+                                        <td className="p-4 text-sm whitespace-nowrap">{ticket.closedDate ? moment(ticket.closedDate).format("YYYY-MM-DD") : "N/A"}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
 
-
-                <table ref={ticketData} id="ticket-data"
-                           className="table-auto m-4 border-separate border-spacing-y-2">
-                        <thead>
-                        <tr>
-                            <th className="text-center w-64" scope="col"> Ticket number</th>
-                            <th className="text-center w-64">Title</th>
-                            <th className="text-center w-64">Description</th>
-                            <th className="text-center w-64">Type</th>
-                            <th className="text-center w-64">Status</th>
-                            <th className="text-center w-64">User Name</th>
-                            <th className="text-center w-64">Employee Id</th>
-                            {/* <th className="text-center w-64">Commands</th> */}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {tickets.slice(pageVisited, pageVisited + ticketPerPage).map((ticket) => (
-                            <tr className="" key={ticket.id}>
-                                <td className="text-center">{ticket.id}</td>
-                                <td className="text-center">{ticket.title}</td>
-                                <td className="text-center">{ticket.description}</td>
-                                <td className="text-center">{ticket.type}</td>
-                                <td className="text-center">{ticket.status}</td>
-                                <td className="text-center">{ticket.user.firstName} {ticket.user.lastName}</td>
-                                <td className="text-center">{ticket.user.employeeId}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                    {/*</ExportToExcel>*/}
+                    {/* Pagination */}
+                    <div className="flex justify-center py-2">
+                        <ReactPaginate
+                            previousLabel={"Previous"}
+                            nextLabel={"Next"}
+                            pageCount={pageCount}
+                            onPageChange={changePage}
+                            containerClassName={"paginationBttns"}
+                            previousLinkClassName={"previousBttn"}
+                            nextLinkClassName={"nextBttn"}
+                            disabledClassName={"paginationDisabled"}
+                            activeClassName={"paginationActive"}
+                        />
+                    </div>
                 </div>
-                <br/>
-                <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    pageCount={pageCount}
-                    onPageChange={changePage}
-                    containerClassName={"paginationBttns"}
-                    previousLinkClassName={"previousBttn"}
-                    nextLinkClassName={"nextBttn"}
-                    disabledClassName={"paginationDisabled"}
-                    activeClassName={"paginationActive"}
-                />
 
             </div>
-
         </div>
 
 
